81ab49216e6932d47f4801b3ef46bd66d90b1a50
i77 <jermaine007@qq.com>
2026-09-15 15:51:01 +0800

    chore(release): v0.2.4

--- tests/test_theme_palettes.py
+++ tests/test_theme_palettes.py
@@ -268,7 +268,7 @@ def test_real_register_theme_via_template_source(template_cleanup: None) -> None
 
 
 def test_version_is_bumped() -> None:
-    assert yate.__version__ == "0.2.3"
+    assert yate.__version__ == "0.2.4"
 
 
 def test_pyproject_keeps_the_single_dynamic_version_source() -> None:
